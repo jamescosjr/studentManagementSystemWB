baad9bbccc80067929a3ce79a72a8f1769c45d48
James Junior <jamescosjr@gmail.com>
2024-10-13 19:57:17 -0300

    refactor: update studentController.js to use repository functions and add error handling

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,68 +1,111 @@
-import { createStudent, listStudents, findStudentByName, findStudentByInscription, findStudentByCourse, deleteStudentById, updateStudentById } from '../service/studentService.js';
+import {
+  create,
+  findAll,
+  findByName,
+  findByInscription,
+  findByCourse,
+  deleteById,
+  updateById,
+} from "../repository/studentRepository.js";
+import { validStudentData } from "../utils/validation.js";
 
-export function createStudentHandler(data) {
+export async function createStudentHandler(req, res) {
+    const student = req.body;
+    const validStudant = validStudentData(student);
+      
+    if (!validStudant) {
+      return res.status(400).json("Invalid student data");
+    }
+  
     try {
-        const student = createStudent(data);
-        console.log('Student registered successfully:', student);
+      const newStudent = await create(student);
+      res.status(201).json(newStudent);
     } catch (error) {
-        console.error('Error registering student:', error.message);
+      res.status(500).json(`Error creating student: ${error.message}`);
     }
-};
+  }
 
-export function listStudentsHandler() {
-    const students = listStudents();
-    console.log('Students:', students);
-};
-
-export function findStudentByNameHandler(name) {
-    const student = findStudentByName(name);
-    if (!student) {
-        console.log('Student not found');
-        return;
-    }
-    console.log('Student found:', student);
-};
-
-export function findStudentByInscriptionHandler(inscription) {
-    const student = findStudentByInscription(inscription);
-    if (!student) {
-        console.log('Student not found');
-        return;
-    }
-    console.log('Student found:', student);
+export async function listStudentsHandler(req, res) {
+  try {
+    const students = await findAll();
+    res.status(200).send(students);
+  } catch (error) {
+    res.status(500).send("Error listing students:", error.message);
+  }
 }
 
-export function listStudentsByCourseHandler(course) {
-    const students = findStudentByCourse(course);
-    if (!students.length) {
-        console.log('No students found');
-        return;
+export async function findStudentByNameHandler(req, res) {
+  try {
+    const name = req.params.name;
+    const student = await findByName(name);
+
+    if (!student) {
+      return res.status(404).send("Student not found");
     }
-    console.log('Students found:', students);
+
+    res.status(200).send(student);
+  } catch (error) {
+    res.status(500).send("Error finding student:", error.message);
+  }
 }
 
-export function deleteStudentHandler(id) {
-    try {
-        const deletedStudent = deleteStudentById(id);
-        if (!deletedStudent) {
-            console.log('Student not found, nothing to delete.');
-            return;
-        }
-        console.log('Student deleted successfully:', deletedStudent);
-    } catch (error) {
-        console.error('Error deleting student:', error.message);
-    }
-};
+export async function findStudentByInscriptionHandler(req, res) {
+  try {
+    const inscription = req.params.inscription;
+    const student = await findByInscription(inscription);
 
-export function updateStudentHandler(id, updatedStudent) {
-    try {
-        const result = updateStudentById(id, updatedStudent);
-        if (!result) {
-            console.log('Student not found, nothing to update.');
-            return;
-        }
-        console.log('Student updated successfully:', result);
-    } catch (error) {
-        console.error('Error updating student:', error.message);
+    if (!student) {
+      return res.status(404).send("Student not found");
     }
+
+    res.status(200).send(student);
+  } catch (error) {
+    res.status(500).send("Error finding student:", error.message);
+  }
+}
+
+export async function listStudentsByCourseHandler(req, res) {
+  try {
+    const course = req.params.course;
+    const students = await findByCourse(course);
+
+    if (!students || students.length === 0) {
+      return res.status(404).send("Students not found");
+    }
+
+    res.status(200).send(students);
+  } catch (error) {
+    res.status(500).send("Error finding students:", error.message);
+  }
+}
+
+export async function deleteStudentHandler(req, res) {
+  try {
+    const id = req.params.id;
+    const student = await deleteById(id);
+
+    if (!student) {
+      return res.status(404).send("Student not found");
+    }
+
+    res.status(200).send("Student deleted successfully");
+  } catch (error) {
+    res.status(500).send("Error deleting student:", error.message);
+  }
+}
+
+export async function updateStudentHandler(req, res) {
+  try {
+    const id = req.params.id;
+    const student = req.body;
+    const updatedStudent = await updateById(id, student);
+
+    if (!updatedStudent) {
+      return res.status(404).send("Student not found");
+    }
+
+    res.status(200).send(updatedStudent);
+  } catch (error) {
+    res.status(500).send("Error updating student:", error.message);
+  }
 }
