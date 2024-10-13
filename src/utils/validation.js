0f4de75364f6afe312c0e879ca3c7f065dbe8adb
James Junior <jamescosjr@gmail.com>
2024-10-13 19:56:09 -0300

    refactor: validation functions and add validStudentData function

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,16 @@
-export function isValidString(str) { typeof str === 'string' && str.trim() !== '' };
+export function isValidString(str) { return typeof str === 'string' && str.trim() !== ''; }
 
-export function isValidYear(year) { typeof year === 'number' && year > 0 };
+export function isValidYear(year) { return typeof year === 'number' && year > 0; }
+
+export function validStudentData(student) {
+    if (!isValidString(student.name)){
+        return false;
+    }
+    if (!isValidYear(student.year)) {
+        return false;
+    }
+    if (!isValidString(student.course)) {
+        return false;
+    }
+    return true;
+}
